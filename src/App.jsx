27af6d8217679494592980537d3d812dc3c9e632
Nida Khan <nidaandsalwakhan@gmail>
2026-03-08 20:13:55 +0500

    project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import EventSyncHomepage from './pages/EventSyncHomepage'
+function App() {
+  return (
+    <>
+      <EventSyncHomepage/>
+      
+    </>
+  )
+}
+
+export default App
